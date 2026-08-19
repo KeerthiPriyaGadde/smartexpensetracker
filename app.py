11478ef4e98818219dd3e381cb3f61ded8cbe724
KeerthiPriyaGadde <keerthichowdary407@gmail.com>
2026-08-19 18:01:47 +0530

    Update secret key to use environment variable

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import date
 
 app = Flask(__name__)
-app.secret_key = "smart_expense_secret_key"
+app.secret_key = os.getenv("SECRET_KEY")
 
 
 # ---------------- DATABASE CONNECTION ----------------
